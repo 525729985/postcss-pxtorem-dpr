c96f726c114d5a933d05d5e0348845c5ac72cd2a
xu <527718651@qq.com>
2018-02-11 13:21:48 +0800

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import Adaptive from './adaptive'
 
 export default postcss.plugin(pkg.name, (options) => {
   return (css, result) => {
+    if (options.exclude) {
+      if (Object.prototype.toString.call(options.exclude) !== '[object RegExp]') {
+        throw new Error('options.exclude should be RegExp!')
+      }
+      if (options.exclude.test(css.source.input.file.match)) {
+        return result.root = css
+      }
+		}
     const adaptiveIns = new Adaptive(options)
     const output = adaptiveIns.parse(css.toString())
     result.root = postcss.parse(output)
