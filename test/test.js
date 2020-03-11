8435a19fe9d199da1b109380f60e312b1e7e7e7e
525729985 <525729985@qq.com>
2020-03-11 14:56:16 +0800

    hairline

--- test/test.js
+++ test/test.js
@@ -30,8 +30,8 @@ describe('integration', function () {
   })
 
   it('rem prop list', function () {
-    var fixture = readFile('test/autorem/fixture.css')
-    var expected = readFile('test/autorem/expected.css')
+    var fixture = readFile('test/px-list/fixture.css')
+    var expected = readFile('test/px-list/expected.css')
     var output = postcss().use(adaptive({
       propList: [ '*', '!border-radius'],
       selectorBlackList: [/^body$/]
